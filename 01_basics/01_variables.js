4762c5f4f91f170ee162f90d6f4d74cebd1596cb
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-20 06:10:11 +0000

    01_variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "krishnasahoo11156@gmail.com"
+var accountPass = "1234"
+accountCity = "Mumbai"
+
+let accountState;
+
+// accountId = 2
+
+accountEmail = "krsnasahoo469@gmail.com"
+accountPass = 11156
+accountCity = "Ney York"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+/*
+NOTE : 
+prefer not to use var because of not recognising the scope, let resolves this issue...
+*/
+
+console.log(accountState)
