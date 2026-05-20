3d602b48ea7c5769e98ad23ccdb07d2756a84424
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-20 07:00:33 +0000

    03_datatypesConversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,9 @@
+let score = "33aba"
+let answer = null
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+let value = Number(answer)
+console.log(value)
