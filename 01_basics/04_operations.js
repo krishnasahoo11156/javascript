6551a59f8bafd1c31e9eb43807daf5ed31e48bb5
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-20 07:48:14 +0000

    04_variable operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,29 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "krishna "
+let str2 = "sahoo"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + "2")  // 12
+console.log("1" + 2)   // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+
+// agr string first h toh sabhi string m convert ho jayega
+// agr number first h toh sabhi number m convert ho jayega
+
+console.log(+true)
+// console.log(true+) => gives error
+
+let num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter)
+
+// should learn about prefix and postfix
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log("2" > 1)
+console.log()
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null > 0)
+console.log(null < 0)
+console.log()
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+console.log()
+// strict check ===
+
+console.log("2" === 2)
