5f4928f61d043bb68ed5680f99041482e57462e2
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-20 06:26:24 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hellooooooooooooooooooo")
+// alert(3 + 3)
+
+// we are using node not web browser 
+
+console.log(3 + 3)
+console.log("Heloooooooo")
+
+let name = "Krishna"
+let age = 19
+let isLoggedIn = false
+let random = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value(its an intentional value showing nothing)
+// undefined => value not assigned(when not know , not assigned value)
+// symbol => for uniquness mostly used in react
+// object => 
+
+    console.log(typeof "Krishna")
+    console.log(typeof random)
+    console.log(typeof null)
+    console.log(typeof undefined)
