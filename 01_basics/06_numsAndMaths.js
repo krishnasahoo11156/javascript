386d6c54ca7b76c1c3916bf3427204a4157694d7
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-21 05:32:11 +0000

    06_numsAndMaths

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const lotsOfDecimal = 1.7665849587;
+console.log(lotsOfDecimal);
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
+
+console.log()
+
+let myNumber = "74";
+// myNumber += 3;
+console.log(myNumber)
+console.log(typeof myNumber);
+myNumber = Number(myNumber) + 3;
+console.log(myNumber)
+
+/*
+
+**	
+Exponent	
+Raises a base number to the exponent power, that is, the base number multiplied by itself, exponent times.	
+5 ** 2 (returns 25, which is the same as 5 * 5).
+
+*/
