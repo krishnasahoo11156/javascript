71c786f2a798b0e70870a75ee1ac44f830c6530e
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-23 05:37:07 +0000

    03_problem of arrays

---
+++ problems/03p_arrays.js
@@ -0,0 +1,14 @@
+/*
+
+1. Create an array of 5 numbers and print:
+first element
+last element
+total length
+
+*/
+
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(arr[4])
+console.log(arr.length)
+console.log(typeof arr)
