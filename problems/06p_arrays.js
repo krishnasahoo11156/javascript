797eb7e010f562a642952140ae7ab2bc73753fd7
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-23 07:21:25 +0000

    06_solving new problem on js

---
+++ problems/06p_arrays.js
@@ -0,0 +1,20 @@
+/*
+
+4. Check whether an element exists using:
+includes()
+
+*/
+
+let myArr = [1,0,-3,484]
+present = myArr.includes(484)
+console.log(present)
+
+console.log(myArr.indexOf(0))
+console.log(myArr.indexOf(10))
+
+console.log()
+
+console.log(typeof myArr)
+let newArr = myArr.join()
+console.log(typeof newArr)
+console.log(typeof myArr)
