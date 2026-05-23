8ac41fd9d02d021beaa373349e84a00a5436f47a
KrishnaSahoo11156 <krishnasahoo11156@gmail.com>
2026-05-23 05:46:59 +0000

    04_problems on arrays

---
+++ problems/04p_arrays.js
@@ -0,0 +1,27 @@
+/*
+
+2. Create an array containing:
+string
+number
+boolean
+object
+Print all values.
+
+*/
+
+let myArr = ["krishna" , 19 , true , {
+    year : 1,
+    income : "10000"
+} ]
+
+console.log(myArr)
+console.log(typeof myArr)
+console.log(typeof myArr[0])
+console.log(typeof myArr[1])
+console.log(typeof myArr[2])
+console.log(typeof myArr[3])
+
+/*
+take aways : 
+the return of arrays is always object , one array can have different types of value in them...
+*/
